3b65cde9c139593dab324d706c194c0f50f484a6
bcorey <benjaminmcorey@gmail.com>
2025-12-29 17:28:01 -0500

    support key combos from single knob action

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::array::IntoIter;
+
 use bsp::entry;
 use bsp::hal;
 use cortex_m::prelude::*;
@@ -21,6 +23,7 @@ use rp2040_hal::gpio::SioInput;
 #[allow(clippy::wildcard_imports)]
 use usb_device::class_prelude::*;
 use usb_device::prelude::*;
+use usbd_human_interface_device::device::keyboard::BootKeyboard;
 use usbd_human_interface_device::page::Keyboard;
 use usbd_human_interface_device::prelude::*;
 
@@ -85,36 +88,36 @@ fn main() -> ! {
         pins.gpio11.into_pull_up_input(),
         pins.gpio12.into_pull_up_input(),
         pins.gpio13.into_pull_up_input(),
-        Keyboard::LeftArrow,
-        Keyboard::RightArrow,
-        Keyboard::ReturnEnter,
+        Input([Keyboard::LeftArrow]),
+        Input([Keyboard::RightArrow]),
+        Input([Keyboard::ReturnEnter]),
     );
 
     let mut lower_knob = Knob::new(
         pins.gpio21.into_pull_up_input(),
         pins.gpio20.into_pull_up_input(),
         pins.gpio19.into_pull_up_input(),
-        Keyboard::DownArrow,
-        Keyboard::UpArrow,
-        Keyboard::Escape,
+        Input([Keyboard::DownArrow]),
+        Input([Keyboard::UpArrow]),
+        Input([Keyboard::Escape]),
     );
 
     let mut bright_knob = Knob::new(
         pins.gpio27.into_pull_up_input(),
         pins.gpio26.into_pull_up_input(),
         pins.gpio22.into_pull_up_input(),
-        Keyboard::X,
-        Keyboard::Y,
-        Keyboard::Z,
+        Input([Keyboard::X]),
+        Input([Keyboard::Y]),
+        Input([Keyboard::Z]),
     );
 
     let mut volume_knob = Knob::new(
         pins.gpio18.into_pull_up_input(),
         pins.gpio17.into_pull_up_input(),
         pins.gpio16.into_pull_up_input(),
-        Keyboard::A,
-        Keyboard::B,
-        Keyboard::F5,
+        Input([Keyboard::A; 3]),
+        Input([Keyboard::B; 3]),
+        Input([Keyboard::LeftControl, Keyboard::LeftShift, Keyboard::E]),
     );
 
     let mut input_count_down = timer.count_down();
@@ -126,13 +129,12 @@ fn main() -> ! {
     loop {
         //Poll the keys every 2ms
         if input_count_down.wait().is_ok() {
-            let keys = [
-                top_knob.update(),
-                lower_knob.update(),
-                bright_knob.update(),
-                volume_knob.update(),
-            ];
-            match keyboard.device().write_report(keys) {
+            let all_keys = top_knob
+                .update()
+                .chain(lower_knob.update())
+                .chain(bright_knob.update())
+                .chain(volume_knob.update());
+            match keyboard.device().write_report(all_keys) {
                 Err(UsbHidError::WouldBlock) => {}
                 Err(UsbHidError::Duplicate) => {}
                 Ok(_) => {}
@@ -170,19 +172,19 @@ fn main() -> ! {
 
 // push, left, right.
 // 4 knobs.
-struct Knob<DT: PinId, CLK: PinId, BTN: PinId> {
+struct Knob<DT: PinId, CLK: PinId, BTN: PinId, const N: usize> {
     encoder: RotaryEncoder<
         StandardMode,
         Pin<DT, FunctionSio<SioInput>, PullUp>,
         Pin<CLK, FunctionSio<SioInput>, PullUp>,
     >,
     button: Pin<BTN, FunctionSio<SioInput>, PullUp>,
-    on_counterclockwise: Keyboard,
-    on_clockwise: Keyboard,
-    on_press: Keyboard,
+    on_counterclockwise: Input<N>,
+    on_clockwise: Input<N>,
+    on_press: Input<N>,
 }
 
-impl<DT, CLK, BTN> Knob<DT, CLK, BTN>
+impl<DT, CLK, BTN, const N: usize> Knob<DT, CLK, BTN, N>
 where
     DT: PinId,
     CLK: PinId,
@@ -192,9 +194,9 @@ where
         dt: Pin<DT, FunctionSio<SioInput>, PullUp>,
         clk: Pin<CLK, FunctionSio<SioInput>, PullUp>,
         btn: Pin<BTN, FunctionSio<SioInput>, PullUp>,
-        on_counterclockwise: Keyboard,
-        on_clockwise: Keyboard,
-        on_press: Keyboard,
+        on_counterclockwise: Input<N>,
+        on_clockwise: Input<N>,
+        on_press: Input<N>,
     ) -> Self {
         let encoder = RotaryEncoder::new(dt, clk).into_standard_mode();
         Self {
@@ -206,15 +208,26 @@ where
         }
     }
 
-    fn update(&mut self) -> Keyboard {
+    fn update(&mut self) -> impl Iterator<Item = Keyboard> {
         let direction = self.encoder.update();
         let btn = self.button.as_input().is_low().unwrap();
         println!("{}", btn);
         match direction {
-            Direction::Anticlockwise => self.on_counterclockwise,
-            Direction::Clockwise => self.on_clockwise,
-            Direction::None if btn => self.on_press,
-            _ => Keyboard::NoEventIndicated,
+            Direction::Anticlockwise => self.on_counterclockwise.clone().into_iter(),
+            Direction::Clockwise => self.on_clockwise.clone().into_iter(),
+            Direction::None if btn => self.on_press.clone().into_iter(),
+            _ => [Keyboard::NoEventIndicated; N].into_iter(),
         }
+    }
+}
+
+#[derive(Clone)]
+struct Input<const N: usize>([Keyboard; N]);
+
+impl<const N: usize> IntoIterator for Input<N> {
+    type Item = Keyboard;
+    type IntoIter = core::array::IntoIter<Self::Item, N>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
     }
 }
